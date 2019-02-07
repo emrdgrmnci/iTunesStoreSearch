5088c899bfb26b93ce154f06da0e0205cb69db11
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2019-02-07 13:00:20 +0300

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Ali Emre Değirmenci on 7.02.2019.
+//  Copyright © 2019 Ali Emre Değirmenci. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
